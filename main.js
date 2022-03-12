fabe9c9524a3b61437f9a3fbbd7810dc7b44a733
Tsubarah <Tsubarah@hotmail.se>
2022-03-12 17:33:01 +0100

    Cleaned out some code from main.js

--- main.js
+++ main.js
@@ -2,14 +2,6 @@ const myLinks = document.querySelector('.myLinks');
 // const links = document.querySelector('.myLinks a');
 const menu = document.querySelector('.mobileMenu');
 
-// toggleMenu = () => {
-//   if (myLinks.style.display === 'block') {
-//     myLinks.style.display = 'none';
-//   } else {
-//     myLinks.style.display = 'block';
-//   }
-// }
-
 const toggleMenu = () => {
   if (myLinks.style.display === 'block') {
     myLinks.style.display = 'none';
