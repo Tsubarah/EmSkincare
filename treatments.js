26e30d32ba246963c426ad43886921dbfd6f6354
Tsubarah <Tsubarah@hotmail.se>
2022-04-03 11:49:02 +0200

    New design and created a hero

---
+++ treatments.js
@@ -0,0 +1,35 @@
+  const treatments = [
+    {
+      name: 'Brows',
+      description: 'Lorem, ipsum dolor.',
+      items: [
+        {
+          itemName: 'Brow Wax & Style',
+          itemDescription: 'Description',
+          price: 99,
+          duration: '20min'
+        }
+      ]
+    }
+  ]
+
+
+
+
+
+// const Treatments = () => (
+//   {treatments.map(treatment => (
+//     <Treatment
+//       name={treatment.name}
+//       description={treatment.description}
+//       items={treatment.items} 
+//     /> 
+//   ))}
+// )
+
+// const Treatment = ({ name, description, items }) => (
+//   <div>
+//     <h4>{name}</h4>
+//     <div>{price}</div>
+//   </div>
+// )
