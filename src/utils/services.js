3c6d6ca93dbab16716650edb0f8640e482314142
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2023-03-06 19:56:59 +0100

    Updated some prices and added foot massage to services

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,236 +1,255 @@
-  export const services = [
-    {
-      section: "facial",
-      name: "Facial & Body Treatments",
-      description: "A 90 minute facial is recommended for costumers in need of longer extraction or a longer massage.",
-      items: [
-        {
-          itemName: "Facial 60",
-          itemDescription: "Includes cleansing, peeling, steam, extraction, massage, mask, cream.",
-          price: 67,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Facial 75",
-          itemDescription: "Includes cleansing, peeling, steam, extraction, massage, mask, cream, brow shaping.",
-          price: 72,
-          duration: "75 min.",
-        },
-        {
-          itemName: "Facial 90",
-          itemDescription: "Includes cleansing, peeling, steam, extraction, massage, mask, cream, brow shaping.",
-          price: 79,
-          duration: "90 min.",
-        },
-        {
-          itemName: "Facemapping",
-          itemDescription: "When Facemapping, we look over your skin and what it needs. Free when buying products in combination with the treatment.",
-          price: 19,
-          duration: "15 min.",
-        },
-        {
-          itemName: "Dermalogica Pro Eye Flash",
-          itemDescription: "A professional eye treatment with chemical peeling. The peeling is powerful enough to reduce fine lines and irregularities, but at the same time adapted not to irritate the thin skin.",
-          price: 25,
-          duration: "15 min.",
-        },
-        {
-          itemName: "Back Treatment",
-          itemDescription: "Includes cleansing, peeling, steam, extraction, massage, mask, cream.",
-          price: 59,
-          duration: "60 min.",
-        },
-      ]
-    },
-    {
-      section: "lashes-brows",
-      name: "Lashes & Brows",
-      description: "Brows can change it all. Try out styling and tinting your brows and see the difference.",
-      items: [
-        {
-          itemName: "Lash Lift 60",
-          itemDescription: "",
-          price: 65,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Lash Lift 75",
-          itemDescription: "With lash or brow tinting.",
-          price: 75,
-          duration: "75 min.",
-        },
-        {
-          itemName: "Lash Lift 90",
-          itemDescription: "With lash and brow tinting including shaping.",
-          price: 85,
-          duration: "90 min.",
-        },
-        {
-          itemName: "Lash & Brow Tinting",
-          itemDescription: "Including brow shaping.",
-          price: 35,
-          duration: "45 min.",
-        },
-        {
-          itemName: "Lash Tinting",
-          itemDescription: "",
-          price: 22,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Brow Tinting",
-          itemDescription: "Including brow shaping.",
-          price: 35,
-          duration: "45 min.",
-        },
-      ],
-    },
-    {
-      section: "massage",
-      name: "Massage",
-      description: "A well needed massage can improve your performance by reducing stress.",
-      items: [
-        {
-          itemName: "Full-Body",
-          itemDescription: "Relaxing Spa-massage.",
-          price: 64,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Full-Body Luxe",
-          itemDescription: "Includes face- and head massage.",
-          price: 89,
-          duration: "90 min.",
-        },
-        {
-          itemName: "Half-Body",
-          itemDescription: "Relaxing half-body (back and legs) massage.",
-          price: 45,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Facial Massage",
-          itemDescription: "",
-          price: 35,
-          duration: "30 min.",
-        },
-      ],
-    },
-    {
-      section: "waxing",
-      name: "Waxing",
-      description: "Our skincare will make you shine.",
-      items: [
-        {
-          itemName: "Full Legs",
-          itemDescription: "",
-          price: 55,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Half Legs",
-          itemDescription: "",
-          price: 35,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Bikini",
-          itemDescription: "",
-          price: 29,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Full Legs & Bikini",
-          itemDescription: "",
-          price: 75,
-          duration: "90 min.",
-        },
-        {
-          itemName: "Half Legs & Bikini",
-          itemDescription: "",
-          price: 48,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Arms",
-          itemDescription: "",
-          price: 30,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Underarms",
-          itemDescription: "",
-          price: 18,
-          duration: "15 min.",
-        },
-        {
-          itemName: "Back",
-          itemDescription: "",
-          price: 35,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Stomach",
-          itemDescription: "",
-          price: 40,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Full Face",
-          itemDescription: "",
-          price: 25,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Specific Part of Face",
-          itemDescription: "",
-          price: 18,
-          duration: "15 min.",
-        },
-      ],
-    },
-    {
-      section: "pedicure",
-      name: "Pedicure",
-      description: "The perfect pedicure can make a flip flop look like a stiletto",
-      items: [
-        {
-          itemName: "Pedicure",
-          itemDescription: "Pedicure without color.",
-          price: 45,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Pedicure Color",
-          itemDescription: "Pedicure with color.",
-          price: 55,
-          duration: "75 min.",
-        },
-      ],
-    },     {
-      section: "specials",
-      name: "Special Treatments",
-      description: "",
-      items: [
-        {
-          itemName: "Pro Power Peel 30",
-          itemDescription: "For you who want a time-efficient and powerful treatment. The peeling is only applied for the face including the lower jaw line.",
-          price: 80,
-          duration: "30 min.",
-        },
-        {
-          itemName: "Pro Power Peel 60",
-          itemDescription: "For you who want both results and experience. The peeling is applied to the décolleté, neck and face. The treatment ends with in-depth intensive treatment including serum, mask and touch therapy.",
-          price: 140,
-          duration: "60 min.",
-        },
-        {
-          itemName: "Microneedling",
-          itemDescription: "A treatment that gives radiance and a smoother skin structure. Microneedling is a treatment where you create a controlled damage to the skin by piercing the skin with extremly thin and sharp needles so the skin can start its normal healing process. An inflammatory condition occurs (redness, swelling and heat). This stimulates the skin to form new collagen (firmness fibers) which gives an improved skin structure. In the treatment we use sterile disposable needles and treatment materials.",
-          price: 150,
-          duration: "75 min.",
-        },
-        
-      ],
-    },
-  ]
+export const services = [
+  {
+    section: "facial",
+    name: "Facial & Body Treatments",
+    description:
+      "A 90 minute facial is recommended for costumers in need of longer extraction or a longer massage.",
+    items: [
+      {
+        itemName: "Facial 60",
+        itemDescription:
+          "Includes cleansing, peeling, steam, extraction, massage, mask, cream.",
+        price: 69,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Facial 75",
+        itemDescription:
+          "Includes cleansing, peeling, steam, extraction, massage, mask, cream, brow shaping.",
+        price: 75,
+        duration: "75 min.",
+      },
+      {
+        itemName: "Facial 90",
+        itemDescription:
+          "Includes cleansing, peeling, steam, extraction, massage, mask, cream, brow shaping.",
+        price: 82,
+        duration: "90 min.",
+      },
+      {
+        itemName: "Facemapping",
+        itemDescription:
+          "When Facemapping, we look over your skin and what it needs. Free when buying products in combination with the treatment.",
+        price: 19,
+        duration: "15 min.",
+      },
+      {
+        itemName: "Dermalogica Pro Eye Flash",
+        itemDescription:
+          "A professional eye treatment with chemical peeling. The peeling is powerful enough to reduce fine lines and irregularities, but at the same time adapted not to irritate the thin skin.",
+        price: 25,
+        duration: "15 min.",
+      },
+      {
+        itemName: "Back Treatment",
+        itemDescription:
+          "Includes cleansing, peeling, steam, extraction, massage, mask, cream.",
+        price: 59,
+        duration: "60 min.",
+      },
+    ],
+  },
+  {
+    section: "lashes-brows",
+    name: "Lashes & Brows",
+    description:
+      "Brows can change it all. Try out styling and tinting your brows and see the difference.",
+    items: [
+      {
+        itemName: "Lash Lift 60",
+        itemDescription: "",
+        price: 65,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Lash Lift 75",
+        itemDescription: "With lash or brow tinting.",
+        price: 75,
+        duration: "75 min.",
+      },
+      {
+        itemName: "Lash Lift 90",
+        itemDescription: "With lash and brow tinting including shaping.",
+        price: 85,
+        duration: "90 min.",
+      },
+      {
+        itemName: "Lash & Brow Tinting",
+        itemDescription: "Including brow shaping.",
+        price: 40,
+        duration: "45 min.",
+      },
+      {
+        itemName: "Lash Tinting",
+        itemDescription: "",
+        price: 25,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Brow Tinting",
+        itemDescription: "Including brow shaping.",
+        price: 25,
+        duration: "45 min.",
+      },
+    ],
+  },
+  {
+    section: "massage",
+    name: "Massage",
+    description:
+      "A well needed massage can improve your performance by reducing stress.",
+    items: [
+      {
+        itemName: "Full-Body",
+        itemDescription: "Relaxing Spa-massage.",
+        price: 70,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Full-Body Luxe",
+        itemDescription: "Includes face- and head massage.",
+        price: 95,
+        duration: "90 min.",
+      },
+      {
+        itemName: "Half-Body",
+        itemDescription: "Relaxing half-body (back and legs) massage.",
+        price: 50,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Facial Massage",
+        itemDescription: "",
+        price: 35,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Foot Massage",
+        itemDescription: "",
+        price: 35,
+        duration: "30 min.",
+      },
+    ],
+  },
+  {
+    section: "waxing",
+    name: "Waxing",
+    description: "Our skincare will make you shine.",
+    items: [
+      {
+        itemName: "Full Legs",
+        itemDescription: "",
+        price: 55,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Half Legs",
+        itemDescription: "",
+        price: 35,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Bikini",
+        itemDescription: "",
+        price: 29,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Full Legs & Bikini",
+        itemDescription: "",
+        price: 75,
+        duration: "90 min.",
+      },
+      {
+        itemName: "Half Legs & Bikini",
+        itemDescription: "",
+        price: 48,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Arms",
+        itemDescription: "",
+        price: 30,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Underarms",
+        itemDescription: "",
+        price: 18,
+        duration: "15 min.",
+      },
+      {
+        itemName: "Back",
+        itemDescription: "",
+        price: 35,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Stomach",
+        itemDescription: "",
+        price: 40,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Full Face",
+        itemDescription: "",
+        price: 25,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Specific Part of Face",
+        itemDescription: "",
+        price: 18,
+        duration: "15 min.",
+      },
+    ],
+  },
+  {
+    section: "pedicure",
+    name: "Pedicure",
+    description:
+      "The perfect pedicure can make a flip flop look like a stiletto",
+    items: [
+      {
+        itemName: "Pedicure",
+        itemDescription: "Pedicure without color.",
+        price: 45,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Pedicure Color",
+        itemDescription: "Pedicure with color.",
+        price: 55,
+        duration: "75 min.",
+      },
+    ],
+  },
+  {
+    section: "specials",
+    name: "Special Treatments",
+    description: "",
+    items: [
+      {
+        itemName: "Pro Power Peel 30",
+        itemDescription:
+          "For you who want a time-efficient and powerful treatment. The peeling is only applied for the face including the lower jaw line.",
+        price: 80,
+        duration: "30 min.",
+      },
+      {
+        itemName: "Pro Power Peel 60",
+        itemDescription:
+          "For you who want both results and experience. The peeling is applied to the décolleté, neck and face. The treatment ends with in-depth intensive treatment including serum, mask and touch therapy.",
+        price: 140,
+        duration: "60 min.",
+      },
+      {
+        itemName: "Microneedling",
+        itemDescription:
+          "A treatment that gives radiance and a smoother skin structure. Microneedling is a treatment where you create a controlled damage to the skin by piercing the skin with extremly thin and sharp needles so the skin can start its normal healing process. An inflammatory condition occurs (redness, swelling and heat). This stimulates the skin to form new collagen (firmness fibers) which gives an improved skin structure. In the treatment we use sterile disposable needles and treatment materials.",
+        price: 150,
+        duration: "75 min.",
+      },
+    ],
+  },
+]
